ca941ca71a01d2e208ce98fca7cc350a4bc9b2de
MaKiPL <makipol@gmail.com>
2018-07-30 11:38:53 +0200

    Added ADD, ADC, CLC + fixed register display + fixed signed DWORD + other

--- AsmMaki_c/CPU.c
+++ AsmMaki_c/CPU.c
@@ -7,7 +7,95 @@ enum CPUMnemonic
 {
 	AND,
 	MOV,
-	XOR
+	XOR,
+	ADD,
+	ADC,
+	CLC,
+
+
+	//TODO below
+	DIV,
+	IDIV,
+	IMUL,
+	MUL,
+
+	POPA,
+	PUSHA, 
+
+	BSWAP,
+	CPUID,
+
+	CLD,
+	CMP,
+	CMPSB,
+	DEC,
+	INC,
+
+	JA,
+	JAE,
+	JB,
+	JBE,
+	JC,
+	JE,
+	JG,
+	JGE,
+	JL,
+	JLE,
+	JNA,
+	JNAE,
+	JNB,
+	JNBE,
+	JNC,
+	JNE,
+	JNG,
+	JNGE,
+	JNL,
+	JNLE,
+	JNO,
+	JNP,
+	JNS,
+	JNZ,
+	JO,
+	JP,
+	JPE,
+	JPO,
+	JS,
+	JZ,
+	JMP,
+
+	LAHF,
+	LEA,
+	LODSB,
+	LODSW,
+	MOVSB,
+	MOVSW,
+	NEG,
+	NOP,
+	NOT,
+	OR,
+	POP,
+	PUSH,
+	POPF,
+	PUSHF,
+	RCL,
+	RCR,
+	REP,
+	REPE,
+	REPNE,
+	REPNZ,
+	REPZ,
+	ROL,
+	ROR,
+	SAHF,
+	SAL,
+	SAR,
+	SCASB,
+	SCASW,
+	SHL,
+	SHR,
+	STC,
+	STD,
+	TEST
 };
 
 enum CPUMnemonic Mnemonic = -1;
@@ -16,7 +104,9 @@ enum CPUMnemonic Mnemonic = -1;
 
 DWORD reg;
 DWORD reg2;
-unsigned long value;
+unsigned int value;
+
+BYTE FLAGS;
 
 extern int IsCPUMnemonic(char c[])
 {
@@ -27,7 +117,22 @@ extern int IsCPUMnemonic(char c[])
 		Mnemonic = MOV;
 	if (!strcmp(c, "XOR"))
 		Mnemonic = XOR;
+	if (!strcmp(c, "ADD"))
+		Mnemonic = ADD;
+	if (!strcmp(c, "ADC"))
+		Mnemonic = ADC;
+	if (!strcmp(c, "CLC"))
+		Mnemonic = CLC;
 	return Mnemonic+1;
+}
+
+void ParseLAHF()
+{
+	SF = FLAGS & 0b10000000;
+	ZF = FLAGS & 0b01000000;
+	AF = FLAGS & 0b00010000;
+	PF = FLAGS & 0b00000100;
+	CF = FLAGS & 0b00000001;
 }
 
 void _AND( char a[])
@@ -65,13 +170,95 @@ void _XOR(char a[])
 }
 
 
+
+void _ADD(char a[])
+{
+	if (bOK)
+	{
+		__asm
+		{
+			MOV EAX, [reg]
+			MOV EBX, [reg2]
+			ADD EAX, EBX
+			MOV[reg], EAX
+
+			//FLAGS
+			LAHF
+			MOV[FLAGS], AH
+		}
+		SetRegister(a, reg);
+	}
+	else
+	{
+		__asm
+		{
+			MOV EAX, [reg]
+			ADD EAX, [value]
+			MOV [reg], EAX
+
+			//FLAGS
+			LAHF
+			MOV[FLAGS], AH
+		}
+		SetRegister(a, reg);
+	}
+	ParseLAHF();
+}
+
+void _ADC(char a[])
+{
+	if (bOK)
+	{
+		__asm
+		{
+			MOV EAX, [reg]
+			MOV EBX, [reg2]
+			ADC EAX, EBX
+			MOV[reg], EAX
+
+			//FLAGS
+			LAHF
+			MOV[FLAGS], AH
+		}
+		SetRegister(a, reg);
+	}
+	else
+	{
+		__asm
+		{
+			MOV EAX, [reg]
+			ADC EAX, [value]
+			MOV[reg], EAX
+
+			//FLAGS
+			LAHF
+			MOV[FLAGS], AH
+		}
+		SetRegister(a, reg);
+	}
+	ParseLAHF();
+}
+
+void _CLC()
+{
+	__asm
+	{
+		clc
+		//FLAGS
+		LAHF
+		MOV[FLAGS], AH
+	}
+	ParseLAHF();
+}
+
+
+
+
 extern void SimulateCPUMnemonic(char command[8][16])
 {
-	if (strlen(command[1]) == 0 || strlen(command[2]) == 0)
-		return;
 	reg = GetRegister(command[1]);
 	reg2 = GetRegister(command[2]);
-	value = strtol(command[2], NULL, 0);
+	value = (unsigned int)strtoul(command[2], NULL, 0);
 
 	switch (Mnemonic)
 	{
@@ -83,6 +270,15 @@ extern void SimulateCPUMnemonic(char command[8][16])
 		break;
 	case XOR:
 		_XOR(command[1]);
+		break;
+	case ADD:
+		_ADD(command[1]);
+		break;
+	case ADC:
+		_ADC(command[1]);
+		break;
+	case CLC:
+		_CLC();
 		break;
 	}
 }
